0659094393f9dc76b2353a4b226d7cd3107cc67d
Aryan Mondkar <aryan.mondkar@gmail.com>
2024-01-22 12:59:54 -0800

    no initialization message, added remote-nvim.lua

--- lua/mondkurry/core/init.lua
+++ lua/mondkurry/core/init.lua
@@ -1,3 +1,3 @@
 require("mondkurry.core.remap")
-print("Hello, this is mondkurry's configuration for neovim!")
-print("For first time setup of Github Copilot run :Copilot setup")
+-- print("Hello, this is mondkurry's configuration for neovim!")
+-- print("For first time setup of Github Copilot run :Copilot setup")
