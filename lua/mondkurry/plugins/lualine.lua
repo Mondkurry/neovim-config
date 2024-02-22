696cafbb7b4c801c426a4903d2d4eddbcfae9a38
Aryan Mondkar <aryan.mondkar@gmail.com>
2024-02-22 12:50:13 -0800

    added wakatime plugin to see code stats. Also created a global lualine to get that setting I was trying to get for a while.

--- lua/mondkurry/plugins/lualine.lua
+++ lua/mondkurry/plugins/lualine.lua
@@ -2,6 +2,24 @@ return {
   "nvim-lualine/lualine.nvim", 
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    
+    -- function to get lsp
+		local clients_lsp = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+
+			local clients = vim.lsp.buf_get_clients(bufnr)
+			if next(clients) == nil then
+				return ""
+			end
+
+			local c = {}
+			for _, client in pairs(clients) do
+				table.insert(c, client.name)
+			end
+			return "  " .. table.concat(c, "|")
+		end
+
+    -- Other Lualine configurations
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
@@ -9,9 +27,11 @@ return {
     lualine.setup({
       options = {
       	theme = "catppuccin",
-	icons_enabled = true,
+	      icons_enabled = true,
         component_separators = '|',
-	section_separators = { left = '', right = '' },
+        globalstatus = true,
+	      section_separators = { left = '', right = '' },
+	      -- section_separators = { left = '', right = '' },
         disabled_filetypes = {},
         always_divide_middle = true,
       },
@@ -25,8 +45,9 @@ return {
           { 'copilot',
   	    show_colors = true,
   	  },
-          { "fileformat" },
+          -- { "fileformat" },
           { "filetype" },
+          { clients_lsp },
         },
       },
     })
